b8b85a9353b2e8ad3d012a3163d9c84d37635b3b
vanshsingh03 <0vanshsingh0@gmail.com>
2026-08-03 14:31:39 +0530

    Brute Force(Traverse)

---
+++ Leetcode/Worksheet-4/Pb-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> nums;
+
+        while (head) {
+            nums.push_back(head->val);
+            head = head->next;
+        }
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            if (nums[left] != nums[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
